18207b323b70a48e830e9a8212b6a3cecff2bf64
yagyaparasar <parasar1971@gmail.com>
2026-02-07 15:19:37 +0530

    pre and post

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -74,6 +74,22 @@ console.log(numbo)  //102
 
 
 
+//prefix and postfix
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
 
 
 
