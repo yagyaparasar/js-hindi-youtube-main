e2ee1f2d6563e9332e4f4c1485805bea803b20ed
yagyaparasar <parasar1971@gmail.com>
2026-02-23 13:44:32 +0530

    functions2

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -1,3 +1,8 @@
+
+//******************************LECTURE : 19 FUNCTIONS 01 *******************************************************/
+
+
+
 //in simple words :Function is Whatever code we have written , it is wrapped in a package and that package can be moved/accessed/used anywhere as many times as we want
 
 console.log("Y");
@@ -98,5 +103,60 @@ function loginUserNews(username = "yagya") {
 
 
 
+//******************************LECTURE : 20 FUNCTIONS 02 *******************************************************/
 
+//Shopping cart :here we dont know know many items user will add...we just have to add their prices irrespective of number of arguments/items
+//rest and spread operator
+function calculateCartPrice(...num1) {     //use of rest operator here 
+    return num1
+}
+
+console.log(calculateCartPrice(1, 2, 3, 4, 5, 6, 78, 8))    //we get array --> [1,2,3,4,5,6,78,8]
+
+
+//VVI INTERVIEW QUESTION
+
+function calculateCartPrice2(val1, val2, ...num1) {
+    return num1
+}
+
+console.log(calculateCartPrice2(1, 2, 4, 5, 6))   //[ 4, 5, 6 ]  --->since 1,2 goes in val1 and val2 and we return only num1
+
+
+
+//OBJECT IN FUNCTIONS
+
+
+const user = {
+    name: "yagya",
+    price: 195
+}
+
+function handleObject(anyobject) {
+    console.log(`The name is ${anyobject.name} and price is ${anyobject.price}`);
+
+}
+
+handleObject(user)   //The name is yagya and price is 195
+
+
+handleObject({
+    name: "yash",                  //we can directly pass object too : The name is yash and price is 123
+    price: 123
+})
+
+
+
+
+//ARRAY IN FUNCTION
+
+const newArray = [1, 2, 3, 4, 5]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+console.log(returnSecondValue(newArray))   //2
+
+console.log(returnSecondValue([1, 21, 3, 4, 5, 3]))   //21
 
